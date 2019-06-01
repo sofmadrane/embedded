dc1a86d065acd4bc4ae0843cba4e26456d6848c5
nboucherulb <nboucher@ulb.ac.be>
2019-06-01 20:44:32 +0200

    debut phase 2

--- phases.py
+++ phases.py
@@ -97,3 +97,77 @@ class Phase1(DefaultPhase):
         self.manager.p71.update_self()
         self.manager.p81.update_self()
         return
+
+
+class Phase2(DefaultPhase):
+    def __init__(self, init_state=_PHASE_DEACTIVATE):
+        DefaultPhase.__init__(self, init_state)
+
+    def update_related_traffic_lights(self):
+        # DONE
+        # tl1 not impacted by p1
+        self.manager.tl1.update_self()
+        self.manager.tl2.update_self()
+        self.manager.tl3.update_self()
+        self.manager.tl4.update_self()
+        self.manager.tl5.update_self()
+        self.manager.tl6.update_self()
+        self.manager.tl7.update_self()
+
+    def update_related_pedestrian_lights(self):
+        # DONE
+        self.manager.pl1.update_self()
+        self.manager.pl2.update_self()
+        self.manager.pl3.update_self()
+
+    def update_related_push_buttons(self):
+        # DONE
+        self.manager.pb1.update_self()
+        self.manager.pb2.update_self()
+        self.manager.pb3.update_self()
+
+    def update_self(self):
+        # DONE
+        # p1 finish
+        if self.state == _PHASE_DEACTIVATE and self.manager.p1.state == _PHASE_FINISH and not self.manager.pb3 and \
+                not (self.manager.pb1 and self.manager.pb3) and not self.manager.f:
+            self.state = _PHASE_ORANGE
+            return
+        # p4 finish
+        if self.state == _PHASE_DEACTIVATE and self.manager.p6.state == _PHASE_FINISH and not self.manager.pb2 and \
+                not self.manager.f:
+            self.state = _PHASE_ORANGE
+            return
+        # p81 finish
+        if self.state == _PHASE_DEACTIVATE and self.manager.p83.state == _PHASE_FINISH and not self.manager.pb2:
+            self.state = _PHASE_ORANGE
+            return
+        # p71 finish
+        if self.state == _PHASE_DEACTIVATE and self.manager.p73.state == _PHASE_FINISH and not self.manager.f:
+            self.state = _PHASE_ORANGE
+            return
+        # p2 red
+        if self.state == _PHASE_ORANGE:
+            self.state = _PHASE_RED
+            return
+        # p2 green
+        if self.state == _PHASE_RED:
+            self.state = _PHASE_GREEN
+            return
+        # none
+        if self.state == _PHASE_GREEN:
+            self.state = _PHASE_FINISH
+            self.activate_next_phase()
+            return
+        # p2 finish
+        if self.state == _PHASE_FINISH:
+            self.state = _PHASE_DEACTIVATE
+            return
+
+    def activate_next_phase(self):
+        # DONE
+        self.manager.p3.update_self()
+        self.manager.p6.update_self()
+        self.manager.p72.update_self()
+        self.manager.p82.update_self()
+        return
