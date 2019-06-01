4f9b29b32af8c5ce85c5905958034266a124cf9b
MaazouzMehdi <skyrim1995@hotmail.fr>
2019-06-01 21:43:51 +0200

    add trafficlights

--- trafficlights.py
+++ trafficlights.py
@@ -257,3 +257,78 @@ class TrafficLight6:
         if self.state == _TL_GREEN and self.manager.p83.state == _PHASE_RED:
             self.state = _TL_RED
             return
+class TrafficLight5:
+    def __init__(self, init_state=_TL_RED):
+        self.state = init_state
+        self.manager = None
+
+    def update_self(self):
+        # DONE
+        # p1_red
+        if self.state == _TL_GREEN and self.manager.p1.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+        # p2_green
+        if self.state == _TL_RED and self.manager.p2.state == _PHASE_GREEN:
+            self.state = _TL_GREEN
+            return
+        # p3_green
+        if self.state == _TL_RED and self.manager.p3.state == _PHASE_GREEN:
+            self.state = _TL_GREEN
+            return
+        # p4_red
+        if self.state == _TL_GREEN and self.manager.p4.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+        # p7_1_red
+        if self.state == _TL_GREEN and self.manager.p71.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+        # p8_3_red
+        if self.state == _TL_GREEN and self.manager.p83.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+
+class TrafficLight7:
+    def __init__(self, init_state=_TL_RED):
+        self.state = init_state
+        self.manager = None
+
+    def update_self(self):
+        # DONE
+        # p8_1_green
+        if self.state == _TL_RED and self.manager.p81.state == _PHASE_GREEN:
+            self.state = _TL_GREEN
+            return
+        # p8_2_green
+        if self.state == _TL_RED and self.manager.p82.state == _PHASE_GREEN:
+            self.state = _TL_GREEN
+            return
+        # p8_3_green
+        if self.state == _TL_RED and self.manager.p83.state == _PHASE_GREEN:
+            self.state = _TL_GREEN
+            return
+        # p1_red
+        if self.state == _TL_GREEN and self.manager.p1.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+        # p2_red
+        if self.state == _TL_GREEN and self.manager.p2.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+        # p3_red
+        if self.state == _TL_GREEN and self.manager.p3.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+        # p4_red
+        if self.state == _TL_GREEN and self.manager.p4.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+        # p5_red
+        if self.state == _TL_GREEN and self.manager.p5.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
+        # p6_red
+        if self.state == _TL_GREEN and self.manager.p6.state == _PHASE_RED:
+            self.state = _TL_RED
+            return
